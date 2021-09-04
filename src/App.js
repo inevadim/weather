ed0a3a46945548f9cb73b9072e4d506c77b15034
inevadim <kvadrocheyk@gmail.com>
2021-09-04 11:07:11 +0300

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+import { useState } from 'react';
+import style from './App.module.scss';
+const api = {
+  key: 'd340ddd7f500656b21b6a44403c2b480',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = e => {
+    if (e.key === 'Enter') {
+      axios
+        .get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+        })
+        .catch(err => {
+          // what now?
+          alert('input correct city');
+        });
+    }
+  };
+
+  const dateBuilder = () => {
+    let date = String(
+      new Date().toLocaleString('en', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      }),
+    ).slice(0, 17);
+    return date;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.app}>
+      <div className={style.wrapInput}>
+        <input
+          type="text"
+          placeholder="search..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
+      </div>
+
+      {typeof weather.data !== 'undefined' ? (
+        <div>
+          <div className={style.item}>
+            city:{weather.data.name}, {weather.data.sys.country}
+          </div>
+          <div className={style.item}>
+            weather: {Math.round(weather.data.main.temp)} c°
+            {/* {weather.data.weather[0].main} */}
+            <img
+              src={'http://openweathermap.org/img/w/' + weather.data.weather[0].icon + '.png'}
+              alt="weather"
+            />
+          </div>
+          <div className={style.item}>date:{dateBuilder()}</div>
+
+          <div className={style.item}></div>
+          <div className={style.item}>wind speed:{weather.data.wind.speed} m/s</div>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
